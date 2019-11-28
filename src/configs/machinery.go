5438455c7dba437af2e7472f6c203cfe14342513
Sofyan <sofyan.saputra@ralali.com>
2019-11-28 12:08:26 +0700

    Add: SQS Queue server

--- src/configs/machinery.go
+++ src/configs/machinery.go
@@ -1,13 +1,36 @@
 package configs
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
 // LoadMachineryConfig load config Function
 func LoadMachineryConfig(path string) (*config.Config, error) {
-	if path == ""{
-		return config.NewFromYaml(path, true)
+	var sqsClient = sqs.New(session.Must(session.NewSession(&aws.Config{
+		Region:      aws.String("ap-southeast-1"),
+		Credentials: credentials.NewStaticCredentials("AKIAJT6VRYLHGO2NMU7Q", "J8ndXGNUr+FyqTnWJAmYjT3G+OaGwkAkdpgb3f6m", ""),
+		HTTPClient: &http.Client{
+			Timeout: time.Second * 120,
+		},
+	})))
+
+	var visibilityTimeout = 20
+	cnf := &config.Config{
+		Broker:        "https://sqs.ap-southeast-1.amazonaws.com/025432561883",
+		DefaultQueue:  "machinery_tasks",
+		ResultBackend: "redis://localhost:6379",
+		SQS: &config.SQSConfig{
+			Client:            sqsClient,
+			VisibilityTimeout: &visibilityTimeout,
+			WaitTimeSeconds:   30,
+		},
 	}
-	return config.NewFromEnvironment(true)
+	return cnf, nil
 }
