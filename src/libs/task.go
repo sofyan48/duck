8c5e5307aa60abcc008b2822e6631ec49249402c
Sofyan <sofyan.saputra@ralali.com>
2019-12-01 02:30:13 +0700

    Add: Dummy register task from yaml

--- src/libs/task.go
+++ src/libs/task.go
@@ -1,17 +1,28 @@
 package libs
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/RichardKnop/machinery/v1"
+	"github.com/sofyan48/duck/src/libs/scheme"
 	"github.com/sofyan48/duck/src/libs/tasks"
 )
 
 // ListTask list task all
-func ListTask(srv *machinery.Server) {
-	// list All Data
-	tasksData := map[string]interface{}{
+func ListTask(srv *machinery.Server, registerTask scheme.RegisterTask) {
+	data := scheme.RegisterTask{}
+	tasksData := make(map[string]interface{})
+	for _, data := range registerTask.Worker.Register {
+		tasksData[data.Action] = data.Name
+		fmt.Println(tasksData)
+	}
+	fmt.Println(reflect.TypeOf(tasks.TaskRequest))
+
+	tasksData1 := map[string]interface{}{
 		"request": tasks.TaskRequest,
 	}
 
-	srv.RegisterTasks(tasksData)
+	srv.RegisterTasks(tasksData1)
 
 }
--- src/libs/tasks/add.go
+++ src/libs/tasks/add.go
@@ -1,7 +1,6 @@
 package tasks
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -14,7 +13,6 @@ func TaskRequest(url ...string) (string, error) {
 }
 
 func getRequest(args []string) string {
-	fmt.Println(args[0])
 	response, err := http.Get(args[0])
 	if err != nil {
 		log.INFO.Println("The HTTP request failed with error %s\n", err)
