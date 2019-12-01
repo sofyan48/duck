5e91373afdd24a11bdddc26db8fc9f4923aab708
Sofyan <sofyan.saputra@ralali.com>
2019-12-01 16:46:59 +0700

    Edit: Auth ACL Controller routes

--- src/server/controller/routes.go
+++ src/server/controller/routes.go
@@ -10,21 +10,11 @@ import (
 // RoutesController | Create Route Controller Rest API
 func RoutesController(r *gin.Engine) {
 	// Get Controller
-	login := new(controller.LoginController)
 	ping := new(controller.PingController)
-
-	// user := new(users.UsersController)
-
-	// Create Routes No Auth Declare Here
-	auth := r.Group("api")
-	{
-		auth.POST("/login", login.LoginUsers)
-	}
 	// Create Routes With Auth Declare Here
-	r.Use(middlewares.AuthACL())
 	//r.Use(middlewares.AuthToken())
 	api := r.Group("api")
 	{
-		api.GET("/ping", ping.Ping)
+		api.GET("/ping", middlewares.AuthACL(), ping.Ping)
 	}
 }
