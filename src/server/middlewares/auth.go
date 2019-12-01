32e45d878b9f4fb3b62ec078e9f057cb20cbc1e6
Sofyan <sofyan.saputra@ralali.com>
2019-12-01 14:44:19 +0700

    Add: Middleware for server

---
+++ src/server/middlewares/auth.go
@@ -0,0 +1,54 @@
+package middlewares
+
+import (
+	"net"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/sofyan48/duck/app/helper"
+	"github.com/sofyan48/duck/app/libs"
+)
+
+// ChekcIPRange Check ACL Addr range for ip and subneting
+func ChekcIPRange(wip []string, addr string) bool {
+
+	for _, ip := range wip {
+		ipfix := strings.ReplaceAll(ip, " ", "")
+		_, ipv4Net, err := net.ParseCIDR(ipfix)
+		if err != nil {
+			logrus.Infof("Ip Not Supported : %s", err)
+		}
+		if ipv4Net.Contains(net.ParseIP(addr)) {
+			return true
+		}
+	}
+	return false
+}
+
+// CheckValidIP Checking Valid IP
+func CheckValidIP(c *gin.Context, ip string) {
+	whitelistaddr := libs.GetEnvVariabel("ACL_ADDR", os.Getenv("ACL_ADDR"))
+	cidr := strings.Split(whitelistaddr, ",")
+	logrus.Infof("IP : %s", ip)
+	if ip == "::1" {
+		c.Next()
+	} else {
+		check := ChekcIPRange(cidr, ip)
+		if check != true {
+			helper.ResponseMsg(c, 401, "Your not Authorize")
+			c.Abort()
+		} else {
+			c.Next()
+		}
+	}
+}
+
+// AuthACL Initial
+func AuthACL() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		CheckValidIP(c, ip)
+	}
+}
